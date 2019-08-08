f8193b8bccfae1c00c6dbea6140a77e5734bfc5d
Alexandra Strekalova <astrekal@thoughtworks.com>
2019-08-08 15:46:38 +0200

    make sure repo is empty before running the tests

--- src/test/scala/VoucherRepositoryTest.scala
+++ src/test/scala/VoucherRepositoryTest.scala
@@ -5,45 +5,53 @@ import akka.stream.ActorMaterializer
 import akka.testkit.TestKit
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.Span
-import org.scalatest.{FunSuite, FunSuiteLike, Matchers}
+import org.scalatest.{BeforeAndAfter, FunSuite, FunSuiteLike, Matchers}
 import org.scalatest.time._
 
 import scala.concurrent.duration._
 import scala.concurrent.Await
 
-class VoucherRepositoryTest extends TestKit(ActorSystem("MySpec")) with FunSuiteLike with Matchers with ScalaFutures {
+class VoucherRepositoryTest extends TestKit(ActorSystem("MySpec")) with FunSuiteLike with Matchers with ScalaFutures with BeforeAndAfter {
 
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-  implicit val patience = PatienceConfig(Span(1, Minute))
+  implicit val patience: PatienceConfig = PatienceConfig(Span(1, Minute))
+
+  var repo: VoucherRepository = _
+
+  before {
+    repo = VoucherRepository()
+    repo.deleteVoucher(1).futureValue
+    repo.deleteVoucher(2).futureValue
+    repo.getVouchers().futureValue shouldBe Seq()
+    println("Repo is empty")
+
+  }
 
   test("testGetVoucher") {
 
-    val repo: VoucherRepository = VoucherRepository()
     val result = repo.getVoucher(1)
     result.futureValue shouldBe None
   }
 
   test("testSaveGetVouchers") {
 
-    val repo: VoucherRepository = VoucherRepository()
-//    repo.getVouchers().futureValue shouldBe Seq()
+    repo.getVouchers().futureValue shouldBe Seq()
 
     repo.saveVoucher(Voucher(1)).futureValue
     repo.saveVoucher(Voucher(2)).futureValue
 
-    repo.getVouchers.futureValue should contain allOf (Voucher(1), Voucher(2))
+    repo.getVouchers().futureValue should contain allOf (Voucher(1), Voucher(2))
 
   }
 
   test("testSaveVoucher") {
 
-    val repo: VoucherRepository = VoucherRepository()
     repo.getVoucher(1).futureValue shouldBe None
 
     val voucher: Voucher = Voucher(1)
     val result = repo.saveVoucher(voucher)
-    result.futureValue shouldBe ()
+    result.futureValue shouldBe Done
 
     val finalResult = repo.getVoucher(1)
     finalResult.futureValue shouldBe Some(Voucher(1))
@@ -52,25 +60,13 @@ class VoucherRepositoryTest extends TestKit(ActorSystem("MySpec")) with FunSuite
 
   test("testDeleteVoucher") {
 
-    val repo: VoucherRepository = VoucherRepository()
-
     val voucher: Voucher = Voucher(1)
     val result = repo.saveVoucher(voucher)
-    result.futureValue shouldBe ()
+    result.futureValue shouldBe Done
 
     val finalResult = repo.getVoucher(1)
     finalResult.futureValue shouldBe Some(Voucher(1))
 
     repo.deleteVoucher(1).futureValue shouldBe Done
   }
-
-  test("fold") {
-
-    val ints = Seq(1,2,3)
-
-    ints.foldLeft(0)((acc, i) => acc + i)
-
-
-  }
-
 }
